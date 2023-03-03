a8d59f27008db1d9f502d3343fe9ce1df14d1fc1
giovanafurlan <giovananelofurlan@gmail.com>
2023-03-03 15:58:59 -0300

    Refatoração geração texto

--- pages/geradorTexto/index.jsx
+++ pages/geradorTexto/index.jsx
@@ -10,12 +10,31 @@ import {
   Input,
   Radio,
   RadioGroup,
+  Text,
   useColorModeValue
 } from "@chakra-ui/react";
 import { useQuill } from "react-quilljs";
 import "quill/dist/quill.snow.css";
 import useTranslation from "next-translate/useTranslation";
 import { getText } from "../../services/getApis";
+import styled from "styled-components";
+
+export const Estilo = styled.div`
+  .ql-toolbar.ql-snow, 
+  .ql-snow .ql-stroke, 
+  .ql-snow .ql-fill,
+  .ql-snow .ql-picker   {
+    border-radius: 20px 20px 0 0;
+    border-color: #8a8686;
+    color: #8a8686;
+    stroke: #8a8686;
+  }
+
+  .ql-container.ql-snow {
+    border-radius: 0 0 20px 20px;
+    border-color: #8a8686;
+  }
+`;
 
 export default function GeradorTextos() {
 
@@ -29,12 +48,12 @@ export default function GeradorTextos() {
   const [visibility, setVisibility] = useState('hidden');
 
   const [numPalavras, setNumPalavras] = useState();
-  const [urlArtigo, setUrlArtigo] = useState('https://webpeak.netlify.app/blogs/6-dicas-para-escrever-conteudo-otimizado-para-seo');
+  const [urlArtigo, setUrlArtigo] = useState('https://webpeak.netlify.app/blogs/a-webpeak-e-a-ferramenta-mais-completa-de-marketing-digital-do-mercado');
   const [nomeEmpresa, setNomeEmpresa] = useState('Webpeak');
-  const [siteEmpresa, setSiteEmpresa] = useState('https://www.webpeak.com.br/');
+  const [siteEmpresa, setSiteEmpresa] = useState('https://www.webpeak.com.br');
 
   // const [text, setText] = useState();
-
+  
   async function handleSubmit() {
 
     setIsLoading(true);
@@ -89,75 +108,84 @@ export default function GeradorTextos() {
   ]
 
   return (
-    <Flex
-      p='10'
-      flexDir={'column'}
-      gap='4'>
-      <FormControl
-        isRequired>
-        <FormLabel>
-          Quantidade de palavras
-        </FormLabel>
-        <RadioGroup
-          colorScheme={'purple'}
-          onChange={setNumPalavras}
-          defaultValue='500'>
-          <Grid
-            templateColumns={'repeat(2,1fr)'}>
-            <Radio
-              value='500'>
-              500
-            </Radio>
-            <Radio
-              value='1000'>
-              1000
-            </Radio>
-            <Radio
-              value='1500'>
-              1500
-            </Radio>
-            <Radio
-              value='2000'>
-              2000
-            </Radio>
-          </Grid>
-        </RadioGroup>
-      </FormControl>
-      {fields.map((item, idx) => (
-        <Field
-          key={idx}
-          isRequired={item.isRequired}
-          id={item.id}
-          title={item.title}
-          value={item.value}
-          onChange={item.onChange} />
-      ))}
-      <Box
-        w='full'>
-        <Button
-          onClick={() => { handleSubmit() }}
-          variant="button-orange"
-          _hover={{
-            bg: "#FFB596",
-          }} >
-          {t('gerar')}
-        </Button>
-      </Box>
-      
-      <Box
-        visibility={visibility}>
-        {isLoading
-          ?
-          <CircularProgress
-            isIndeterminate />
-          :
-          <></>
-        }
+      <Flex
+        flexDir={'column'}
+        gap='4'>
+        <FormControl
+          isRequired>
+          <FormLabel>
+            Quantidade de palavras
+          </FormLabel>
+          <RadioGroup
+            colorScheme={'purple'}
+            onChange={setNumPalavras}>
+            <Grid
+              templateColumns={'repeat(4,1fr)'}
+              gap='2'
+              w='min-content'>
+              <Radio
+                value='500'>
+                500
+              </Radio>
+              <Radio
+                value='1000'>
+                1000
+              </Radio>
+              <Radio
+                value='1500'>
+                1500
+              </Radio>
+              <Radio
+                value='2000'>
+                2000
+              </Radio>
+            </Grid>
+          </RadioGroup>
+        </FormControl>
+        {fields.map((item, idx) => (
+          <Field
+            key={idx}
+            isRequired={item.isRequired}
+            id={item.id}
+            title={item.title}
+            value={item.value}
+            onChange={item.onChange} />
+        ))}
         <Box
-        ref={quillRef}
-        h={"96"} />
-      </Box>
-    </Flex>
+          w='full'>
+          <Button
+            onClick={() => { handleSubmit() }}
+            variant="button-orange"
+            _hover={{
+              bg: "#FFB596",
+            }} >
+            {t('gerar')}
+          </Button>
+        </Box>
+        <Box
+          visibility={visibility}>
+          {isLoading
+            ?
+            <Flex
+            gap='4'
+            align={'center'}
+            mb='4'>
+              <CircularProgress
+                isIndeterminate />
+              <Text>
+                {t('aguarde')}
+                </Text>
+            </Flex>
+            :
+            <></>
+          }
+          <Estilo>
+            <Box
+              ref={quillRef}
+              h={"96"} />
+          </Estilo>
+        </Box>
+      </Flex>
   )
 }
 
