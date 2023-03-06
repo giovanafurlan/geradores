95a201343b3953012615278cac2937c427247dbb
giovanafurlan <giovananelofurlan@gmail.com>
2023-03-06 15:15:17 -0300

    Menu

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,9 @@
-import { useState } from "react";
-import {
-  Button,
-  ButtonGroup,
-  Container,
-  Flex
-} from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import Menu from './components/Menu';
+
 export default function GeradorTitle() {
 
-  const router = useRouter();
-
   return (
-    <Container>
-      <Flex
-        flexDir={'column'}
-        gap='4'>
-        <Button
-          onClick={() => router.push('/geradorTitle')}>
-          Gerador Títulos e Descrição
-        </Button>
-        <Button
-          onClick={() => router.push('/geradorAds')}>
-          Gerador Anúncios
-        </Button>
-        <Button
-          onClick={() => router.push('/geradorTexto')}>
-          Gerador Texto
-        </Button>
-      </Flex>
-    </Container>
+    <Menu>
+    </Menu>
   )
 }
